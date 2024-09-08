c08145a4d0e8cd2ddad7fa3f44502d7be32dbca3
Caio Murillo Oliveira <caio@BRUDDENPO.LOCAL>
2024-09-08 15:33:18 -0300

    atualizar app.js - projeto final

---
+++ js/app.js
@@ -0,0 +1,45 @@
+function comprar(){
+  let tipo = document.getElementById('tipo-ingresso').value;
+  let qtd = document.getElementById('qtd').value;
+
+  if(tipo == 'pista'){
+    comprarPista(qtd);
+  } else if( tipo == 'superior'){
+    comprarSuperior(qtd);
+  } else {
+    comprarInferior(qtd);
+  }
+}
+
+function comprarPista(qtd){
+  let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+  if(qtd > qtdPista){
+    alert('Quantidade indisponível para pista!')
+  } else {
+    qtdPista = qtdPista - qtd;
+    document.getElementById('qtd-pista').textContent = qtdPista;
+    alert('Compra realizada com sucesso!')
+  }
+}
+
+function comprarSuperior(qtd){
+  let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+  if(qtd > qtdSuperior){
+    alert('Quantidade indisponível para cadeira superior!')
+  } else {
+    qtdSuperior = qtdSuperior - qtd;
+    document.getElementById('qtd-superior').textContent = qtdSuperior;
+    alert('Compra realizada com sucesso!')
+  }
+}
+
+function comprarInferior(qtd){
+  let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+  if(qtd > qtdInferior){
+    alert('Quantidade indisponível para cadeira inferior!')
+  } else {
+    qtdInferior = qtdInferior - qtd;
+    document.getElementById('qtd-inferior').textContent = qtdInferior;
+    alert('Compra realizada com sucesso!')
+  }
+}
